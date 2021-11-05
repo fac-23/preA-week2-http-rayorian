60f07f27825f58fcc8b6748cb10551fcf5467685
Orian <77785778+OrianP@users.noreply.github.com>
2021-11-05 13:41:09 +0000

    Get crimes create function to fetch crime data at street location

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-// access police API
+// Use navigator object to retrieve current user coordiantes
+const lat = 51.591577;
+const lng = -0.0201425;
+// return a url to pass into the getCrimes function
+
+// request street crime data from police API using user coordinates
+
+const userLocation = `https://data.police.uk/api/crimes-street/all-crime?lat=${lat}&lng=${lng}`;
+
+function getCrimes(userLocation) {
+    fetch(userLocation)
+    .then((response) => response.json())
+    .then(console.log);
+} 
+
+// getCrimes(userLocation);
+
+// try making neighbourhood boundary request
+fetch(`https://data.police.uk/api/leicestershire/NC04/boundary`)
+.then((response) => response.json())
+.then(console.log);
