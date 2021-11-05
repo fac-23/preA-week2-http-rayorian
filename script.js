9b7d675b81749e89db409a5f91f6ef0a7d604225
raythompsonwebdev <ray_thomp@hushmail.com>
2021-11-05 21:05:09 +0000

    added promise all to get data for both mapbox and police api's in one response added conditional to test whether browser supports mapbox.

---
+++ script.js
@@ -0,0 +1,70 @@
+
+let container = document.querySelector('#text-container');
+
+// if browser does not support map box.
+if (!mapboxgl.supported()) {
+
+    alert('Your browser does not support Mapbox GL');
+    
+} else {
+    // sign up for account at mapbox to get secret key and public key to test. https://docs.mapbox.com/help/getting-started/
+    mapboxgl.accessToken = '';
+
+    const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',           
+        center: [-79.4512, 43.6568],
+        zoom: 13
+    });
+                    
+    // Initialize the GeolocateControl.
+    const geolocate = new mapboxgl.GeolocateControl({
+        positionOptions: {
+            enableHighAccuracy: true
+        },
+        trackUserLocation: true,
+        showUserHeading: true
+    });
+
+    // Add the control to the map.
+    map.addControl(geolocate);
+    // Set an event listener that fires
+    // when a geolocate event occurs.
+    geolocate.on('geolocate', () => {
+        console.log('A geolocate event has occurred.');
+    });
+
+    // Add the control to the map.
+    const geocoder = new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl
+    });
+
+    //append search text input to div element with id geocoder
+    document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
+
+
+    //police api
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+    };
+
+    // send request to police api
+    const fetchPolice = fetch(`https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592`, requestOptions);
+
+    // send request to mapbox api
+    const fetchMap = fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/chester.json?proximity=-74.70850,40.78375&access_token=${mapboxgl.accessToken}`)
+
+    // Use promise all to get data from both apis
+    //promise all accepts array. 
+    Promise.all([fetchPolice,fetchMap])
+    .then(values => {
+        // loop over values in array of both responses. convert each element into array to json. return converted array of reponses.  
+        return Promise.all(values.map(r => r.json()));
+    }) // value variable is now an array of values from both responses
+    .then(value => console.log(value));
+    
+    
+
+}
