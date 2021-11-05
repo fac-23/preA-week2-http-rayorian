a9a7dd482939b8af0c0dd82beb1fba2dd9a3f12f
raythompsonwebdev <ray_thomp@hushmail.com>
2021-11-05 22:45:33 +0000

    removed api key

--- script.js
+++ script.js
@@ -18,7 +18,7 @@
 
         } else {
             // sign up for account at mapbox to get secret key and public key to test. https://docs.mapbox.com/help/getting-started/
-            mapboxgl.accessToken = 'pk.eyJ1IjoicmF5b3JpYW5mYWMyMyIsImEiOiJja3ZrNWYxcDEwYXQxMzJrbHc2cG9lMnlsIn0.TINYt-VPvenBz1wEAUCeQg';
+            mapboxgl.accessToken = '';
 
             const map = new mapboxgl.Map({
                 container: 'map',
