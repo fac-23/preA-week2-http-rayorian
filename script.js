956ba68041284fe5519cc3fa5b48539938701bf5
raythompsonwebdev <ray_thomp@hushmail.com>
2021-11-05 22:41:51 +0000

    added navaigator object removed geolocator control for now

--- script.js
+++ script.js
@@ -1,74 +1,90 @@
+(function(){
 
-let container = document.querySelector('#text-container');
+  if (navigator.geolocation) {
 
-// if browser does not support map box.
-if (!mapboxgl.supported()) {
+      navigator.geolocation.getCurrentPosition(function(position){
 
-    alert('Your browser does not support Mapbox GL');
-    
-} else {
-    // sign up for account at mapbox to get secret key and public key to test. https://docs.mapbox.com/help/getting-started/
-    mapboxgl.accessToken = 'pk.eyJ1IjoicmF5b3JpYW5mYWMyMyIsImEiOiJja3ZrNWYxcDEwYXQxMzJrbHc2cG9lMnlsIn0.TINYt-VPvenBz1wEAUCeQg';
+        let lat = position.coords.latitude;
+        let lon = position.coords.longitude;
 
-    const map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v11',           
-        center: [-79.4512, 43.6568],
-        zoom: 13
+        console.log(lat, lon)
+
+        let container = document.querySelector('#text-container');
+
+        // if browser does not support map box.
+        if (!mapboxgl.supported()) {
+
+            alert('Your browser does not support Mapbox GL');
+
+        } else {
+            // sign up for account at mapbox to get secret key and public key to test. https://docs.mapbox.com/help/getting-started/
+            mapboxgl.accessToken = 'pk.eyJ1IjoicmF5b3JpYW5mYWMyMyIsImEiOiJja3ZrNWYxcDEwYXQxMzJrbHc2cG9lMnlsIn0.TINYt-VPvenBz1wEAUCeQg';
+
+            const map = new mapboxgl.Map({
+                container: 'map',
+                style: 'mapbox://styles/mapbox/streets-v11',
+                center: [`${lon}`, `${lat}`],
+                zoom: 15
+            });
+
+            // Add the control to the map.
+            const geocoder = new MapboxGeocoder({
+                accessToken: mapboxgl.accessToken,
+                mapboxgl: mapboxgl
+            });
+
+            //append search text input to div element with id geocoder
+            document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
+
+
+            //police api request data object
+            var requestOptions = {
+                method: 'GET',
+                redirect: 'follow'
+            };
+
+            // send request to police api
+            const fetchPolice = fetch(`https://data.police.uk/api/crimes-street/all-crime?lat=${lat}&lng=${lon}`, requestOptions);
+
+            // send request to mapbox api
+            const fetchMap = fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${lon},${lat}.json?&access_token=${mapboxgl.accessToken}`)
+
+            // Use promise all to get data for both apis
+            // promise all accepts array.
+            Promise.all([fetchPolice,fetchMap])
+            .then(values => {
+                // loop over values in array of both responses. convert each element in array to json. return converted array of responses.
+                return Promise.all(values.map(element => element.json()));
+            })
+            .then( ([policeData, mapData]) => {
+                // value variable is now an array of  json values from both responses. value can also be deconstructed to
+                console.log(policeData)
+                let html = '';
+
+                for(element of policeData ){
+
+                    html = `
+                            <h2>${element.category}</h2>
+                            <h2>${element.location.longitude}</h2>
+                            <h2>${element.location.latitude}</h2>
+                            <h2>${element.month}</h2>
+                            <h2>${element.location.street.name}</h2>
+                        `;
+
+                }
+                container.innerHTML = html;
+
+
+            });
+
+
+        }
     });
-                    
-    // Initialize the GeolocateControl.
-    const geolocate = new mapboxgl.GeolocateControl({
-        positionOptions: {
-            enableHighAccuracy: true
-        },
-        trackUserLocation: true,
-        showUserHeading: true
-    });
+	}
+	else {
 
-    // Add the control to the map.
-    map.addControl(geolocate);
-    // Set an event listener that fires
-    // when a geolocate event occurs.
-    geolocate.on('geolocate', () => {
-        console.log('A geolocate event has occurred.');
-    });
+		alert("Geolocation is not supported by this browser.");
 
-    // Add the control to the map.
-    const geocoder = new MapboxGeocoder({
-        accessToken: mapboxgl.accessToken,
-        mapboxgl: mapboxgl
-    });
+	}
 
-    //append search text input to div element with id geocoder
-    document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
-
-
-    //police api
-    var requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-    };
-
-    // send request to police api
-    const fetchPolice = fetch(`https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592`, requestOptions);
-
-    // send request to mapbox api
-    const fetchMap = fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/chester.json?proximity=-74.70850,40.78375&access_token=${mapboxgl.accessToken}`)
-
-    // Use promise all to get data for both apis
-    // promise all accepts array. 
-    Promise.all([fetchPolice,fetchMap])
-    .then(values => {
-        // loop over values in array of both responses. convert each element in array to json. return converted array of responses.  
-        return Promise.all(values.map(element => element.json()));
-    }) 
-    .then( ([policeData, mapData]) => {
-        // value variable is now an array of  json values from both responses. value can also be deconstructed to 
-        console.log(policeData, mapData)
-    })
-    
-    
-    
-
-}
+})();
