554ef76362ef3cd318c1210e91028aa68485d98f
raythompsonwebdev <ray_thomp@hushmail.com>
2021-11-06 02:15:39 +0000

    added html select tag with crime categories as options

--- script.js
+++ script.js
@@ -2,83 +2,102 @@
 
   if (navigator.geolocation) {
 
-      navigator.geolocation.getCurrentPosition(function(position){
+    navigator.geolocation.getCurrentPosition(function(position){
 
-        let lat = position.coords.latitude;
-        let lon = position.coords.longitude;
+			let lat = position.coords.latitude;
+			let lon = position.coords.longitude;
 
-        console.log(lat, lon)
+			console.log(lat, lon)
 
-        let container = document.querySelector('#text-container');
+			let container = document.querySelector('#text-container');
 
-        // if browser does not support map box.
-        if (!mapboxgl.supported()) {
-
-            alert('Your browser does not support Mapbox GL');
-
-        } else {
-            // sign up for account at mapbox to get secret key and public key to test. https://docs.mapbox.com/help/getting-started/
-            mapboxgl.accessToken = '';
-
-            const map = new mapboxgl.Map({
-                container: 'map',
-                style: 'mapbox://styles/mapbox/streets-v11',
-                center: [`${lon}`, `${lat}`],
-                zoom: 15
-            });
-
-            // Add the control to the map.
-            const geocoder = new MapboxGeocoder({
-                accessToken: mapboxgl.accessToken,
-                mapboxgl: mapboxgl
-            });
-
-            //append search text input to div element with id geocoder
-            document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
+			let select = document.getElementById('crimes');
 
 
-            //police api request data object
-            var requestOptions = {
-                method: 'GET',
-                redirect: 'follow'
-            };
+			// if browser does not support map box.
+			if (!mapboxgl.supported()) {
 
-            // send request to police api
-            const fetchPolice = fetch(`https://data.police.uk/api/crimes-street/all-crime?lat=${lat}&lng=${lon}`, requestOptions);
+					alert('Your browser does not support Mapbox GL');
 
-            // send request to mapbox api
-            const fetchMap = fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${lon},${lat}.json?&access_token=${mapboxgl.accessToken}`)
+			} else {
+					// sign up for account at mapbox to get secret key and public key to test. https://docs.mapbox.com/help/getting-started/
+					mapboxgl.accessToken = 'pk.eyJ1IjoicmF5b3JpYW5mYWMyMyIsImEiOiJja3ZrNWYxcDEwYXQxMzJrbHc2cG9lMnlsIn0.TINYt-VPvenBz1wEAUCeQg';
 
-            // Use promise all to get data for both apis
-            // promise all accepts array.
-            Promise.all([fetchPolice,fetchMap])
-            .then(values => {
-                // loop over values in array of both responses. convert each element in array to json. return converted array of responses.
-                return Promise.all(values.map(element => element.json()));
-            })
-            .then( ([policeData, mapData]) => {
-                // value variable is now an array of  json values from both responses. value can also be deconstructed to
-                console.log(policeData)
-                let html = '';
+					const map = new mapboxgl.Map({
+							container: 'map',
+							style: 'mapbox://styles/mapbox/streets-v11',
+							center: [`${lon}`, `${lat}`],
+							zoom: 15
+					});
 
-                for(element of policeData ){
+					// Add the control to the map.
+					const geocoder = new MapboxGeocoder({
+							accessToken: mapboxgl.accessToken,
+							mapboxgl: mapboxgl
+					});
 
-                    html = `
-                            <h2>${element.category}</h2>
-                            <h2>${element.location.longitude}</h2>
-                            <h2>${element.location.latitude}</h2>
-                            <h2>${element.month}</h2>
-                            <h2>${element.location.street.name}</h2>
-                        `;
-
-                }
-                container.innerHTML = html;
+					//append search text input to div element with id geocoder
+					document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
 
 
-            });
+					//police api request data object
+					var requestOptions = {
+							method: 'GET',
+							redirect: 'follow'
+					};
+
+					// send request to police api
+					const fetchPolice = fetch(`https://data.police.uk/api/crimes-street/all-crime?lat=${lat}&lng=${lon}&date=2020-09`, requestOptions);
+
+					// send request to mapbox api
+					const fetchMap = fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${lon},${lat}.json?&access_token=${mapboxgl.accessToken}`)
+
+					// Use promise all to get data for both apis
+					// promise all accepts array.
+					Promise.all([fetchPolice,fetchMap])
+					.then(values => {
+							// loop over values in array of both responses. convert each element in array to json. return converted array of responses.
+							return Promise.all(values.map(element => element.json()));
+					})
+					.then( ([policeData, mapData]) => {
+						// value variable is now an array of  json values from both responses. value can also be deconstructed to
+						//console.log(policeData)
 
 
-        }
+						window.addEventListener('onMouseUp', (event) => {
+							preventDefault(event);
+							var el = document.getElementsByClassname('title');
+							console.log(el)
+						});
+
+						select.addEventListener('change', (event) => {
+
+							//console.log( event.target.value);
+
+							for( let police of policeData){
+
+								if(event.target.value === police.category ){
+
+									console.log(police.category);
+									//console.log(select.options[select.selectedIndex].text); // 1
+									// Create a element
+
+									btn = document.createElement("H2");
+									btn.setAttribute("class", "title")
+									btn.innerHTML = `${police.category}`;
+									// append element to container
+									container.appendChild(btn);
+
+								}
+
+							}
+
+						});
+
+					});
+
+
+			}
     });
 	}
 	else {
