d05121a28ab2e584b66c5166b59dec409949fdeb
raythompsonwebdev <ray_thomp@hushmail.com>
2021-11-06 03:24:15 +0000

    added conditional to replace existing element selction with new selction

--- script.js
+++ script.js
@@ -6,97 +6,79 @@
 
 			let lat = position.coords.latitude;
 			let lon = position.coords.longitude;
-
-			console.log(lat, lon)
-
 			let container = document.querySelector('#text-container');
-
 			let select = document.getElementById('crimes');
-
 
 			// if browser does not support map box.
 			if (!mapboxgl.supported()) {
-
 					alert('Your browser does not support Mapbox GL');
 
-			} else {
-					// sign up for account at mapbox to get secret key and public key to test. https://docs.mapbox.com/help/getting-started/
-					mapboxgl.accessToken = 'pk.eyJ1IjoicmF5b3JpYW5mYWMyMyIsImEiOiJja3ZrNWYxcDEwYXQxMzJrbHc2cG9lMnlsIn0.TINYt-VPvenBz1wEAUCeQg';
+			}else {
 
-					const map = new mapboxgl.Map({
-							container: 'map',
-							style: 'mapbox://styles/mapbox/streets-v11',
-							center: [`${lon}`, `${lat}`],
-							zoom: 15
-					});
+				// sign up for account at mapbox to get secret key and public key to test. https://docs.mapbox.com/help/getting-started/
+				mapboxgl.accessToken = '';
 
-					// Add the control to the map.
-					const geocoder = new MapboxGeocoder({
-							accessToken: mapboxgl.accessToken,
-							mapboxgl: mapboxgl
-					});
+				const map = new mapboxgl.Map({
+						container: 'map',
+						style: 'mapbox://styles/mapbox/streets-v11',
+						center: [`${lon}`, `${lat}`],
+						zoom: 15
+				});
 
-					//append search text input to div element with id geocoder
-					document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
+				//police api request data object
+				var requestOptions = {
+						method: 'GET',
+						redirect: 'follow'
+				};
 
+				// send request to police api
+				const fetchPolice = fetch(`https://data.police.uk/api/crimes-street/all-crime?lat=${lat}&lng=${lon}&date=2020-09`, requestOptions);
 
-					//police api request data object
-					var requestOptions = {
-							method: 'GET',
-							redirect: 'follow'
-					};
+				// send request to mapbox api
+				const fetchMap = fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${lon},${lat}.json?&access_token=${mapboxgl.accessToken}`)
 
-					// send request to police api
-					const fetchPolice = fetch(`https://data.police.uk/api/crimes-street/all-crime?lat=${lat}&lng=${lon}&date=2020-09`, requestOptions);
+				// Use promise all to get data for both apis
+				// promise all accepts array.
+				Promise.all([fetchPolice,fetchMap])
+				.then(values => {
+						// loop over values in array of both responses. convert each element in array to json. return converted array of responses.
+						return Promise.all(values.map(element => element.json()));
+				})
+				.then( ([policeData, mapData]) => {	// deconstruct value variable array of json values from both responses.
 
-					// send request to mapbox api
-					const fetchMap = fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${lon},${lat}.json?&access_token=${mapboxgl.accessToken}`)
+					// event listener for crime select element
+					select.addEventListener('change', (event) => {
 
-					// Use promise all to get data for both apis
-					// promise all accepts array.
-					Promise.all([fetchPolice,fetchMap])
-					.then(values => {
-							// loop over values in array of both responses. convert each element in array to json. return converted array of responses.
-							return Promise.all(values.map(element => element.json()));
-					})
-					.then( ([policeData, mapData]) => {
-						// value variable is now an array of  json values from both responses. value can also be deconstructed to
-						//console.log(policeData)
+						//get all elements created if they already exist
+						let list = document.getElementsByClassName('title');
 
+						//if elements already exists remove them to be replaced by new elements.
+						if(list){
+							Array.from(list).forEach(e => e.remove() );
+						}
 
-						window.addEventListener('onMouseUp', (event) => {
-							preventDefault(event);
-							var el = document.getElementsByClassname('title');
-							console.log(el)
-						});
+						//loop over police data return from api
+						for( let police of policeData){
 
-						select.addEventListener('change', (event) => {
+							//compare event target value to police api value
+							// thye are the same create element
+							if(event.target.value === police.category ){
 
-							//console.log( event.target.value);
+								console.log(police.category);
 
-							for( let police of policeData){
-
-								if(event.target.value === police.category ){
-
-									console.log(police.category);
-									//console.log(select.options[select.selectedIndex].text); // 1
-									// Create a element
-
-									btn = document.createElement("H2");
-									btn.setAttribute("class", "title")
-									btn.innerHTML = `${police.category}`;
-									// append element to container
-									container.appendChild(btn);
-
-								}
+								// Create a element
+								btn = document.createElement("H2");
+								// add id
+								btn.setAttribute("class", "title");
+								// add text
+								btn.innerHTML = `${police.category}`;
+								// append element to container
+								container.appendChild(btn);
 
 							}
-
-						});
-
+						}
 					});
-
-
+				});
 			}
     });
 	}
