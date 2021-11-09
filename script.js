b5fff63b5cd907330821f2faf16749c31204d787
raythompsonwebdev <ray_thomp@hushmail.com>
2021-11-09 18:26:52 +0000

    added mapbox api key - resticted to githubpage url

--- script.js
+++ script.js
@@ -17,7 +17,7 @@
 
 				var currentMarkers=[];
 				// sign up for account at mapbox to get secret key and public key to test. https://docs.mapbox.com/help/getting-started/
-				mapboxgl.accessToken = 'pk.eyJ1IjoicmF5b3JpYW5mYWMyMyIsImEiOiJja3ZrNWYxcDEwYXQxMzJrbHc2cG9lMnlsIn0.TINYt-VPvenBz1wEAUCeQg';
+				mapboxgl.accessToken = 'pk.eyJ1IjoicmF5b3JpYW5mYWMyMyIsImEiOiJja3Zwc25xNXAyY2VoMm50a2swYnp2dXFhIn0.lKM6McyWmA7lIHSSVQywhA';
 
 				const map = new mapboxgl.Map({
 						container: 'map',
