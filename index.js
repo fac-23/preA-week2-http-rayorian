36ead5f2aa434a1e4c8acd790de8936f4aee9ea6
raythompsonwebdev <raymond.thompson@raythompsonwebdev.co.uk>
2023-12-11 19:55:45 +0000

    updated environment secret

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const runMap = () => {
 			const currentMarkers = [];
 			// sign up for account at mapbox to get secret key and public key to test. https://docs.mapbox.com/help/getting-started/
 			// eslint-disable-next-line no-undef
-			mapboxgl.accessToken = `pk.eyJ1IjoicmF5b3JpYW5mYWMyMyIsImEiOiJjbHB5aXFrZnEwdnY3MmtwZm54eDU0czUzIn0.AzSU55jf1uL-JlgLd9fEYQ`;
+			mapboxgl.accessToken = `pk.eyJ1IjoicmF5b3JpYW5mYWMyMyIsImEiOiJjbHExYXNteWYwN2l1MnJwa3NvMGg0Mng5In0.tOZS7_ScVbmla4rwg0VTxA`;
 			// eslint-disable-next-line no-undef
 			// mapboxgl.accessToken = process.env.API_KEY;
 
