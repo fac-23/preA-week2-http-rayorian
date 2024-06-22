a8e6303fa1b5b478f284bd4c1115d8909375c183
raythompsonwebdev <raymond.thompson@raythompsonwebdev.co.uk>
2024-06-22 11:07:05 +0100

    update

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ const runMap = () => {
 			const currentMarkers = [];
 			// sign up for account at mapbox to get secret key and public key to test. https://docs.mapbox.com/help/getting-started/
 			// eslint-disable-next-line no-undef
-			mapboxgl.accessToken = ``;
+			mapboxgl.accessToken =
+				"pk.eyJ1IjoicmF5b3JpYW5mYWMyMyIsImEiOiJjbHNodDBna2ExMmFuMmxvdWI2MTkyN25oIn0.Lg5ggPjT5EUhMMBe7J-0eg";
+
 			// eslint-disable-next-line no-undef
 			// mapboxgl.accessToken = process.env.API_KEY;
 
